0e254c34a1404fea513a21280803cccd19cb61ca
jordyf15 <jordyferdian88@gmail.com>
2022-02-24 13:21:20 +0700

    changed icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import cvIcon from '../images/cv-icon.png';
 
 class Header extends React.Component{
     render(){
         return(
             <header>
+                <img src={cvIcon} alt='cv'/>
                 <h1>CV Creator</h1>
             </header>
         );
